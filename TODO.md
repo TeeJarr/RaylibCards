fc8d647ac13ba83aa6a4d097390066f0fc3549fd
TeeJarr <tjreyn03@gmail.com>
2025-03-31 13:48:28 -0700

    centered cards on the screen for both even and odd numbered hand sizes

--- TODO.md
+++ TODO.md
@@ -8,8 +8,11 @@
 - [ ] make the cards graphical for the table and the cards
     - [ ] make a draw card function using rectangles, text for the Values, and images of the suits
         - [x] Draw Rectanlges
-        - [x] Draw Text For the Value
+        - [ ] Draw Text For the Value
+            - [ ] make bottom text rotate
+            - [x] draw top text
         - [ ] Draw Image for the suits
+            - [x] use  text for suits
     - [ ] make the whole group of cards center on the screen (middle card in odd group is center and the center of the gap is 
     the middle of the screen for even number of cards)
     - [ ] animate a hover effect for the cards to make them easier to see
@@ -17,7 +20,7 @@
 - [ ] import a custom font to use for the GUI
 - [ ] add specific gameplay functionality
     - [ ] add splitting
-    - [ ] add doublig down
+    - [ ] add doubling down
     - [ ] add insurance
 - [ ] hide the dealer's second card 
 - [ ] add probability features
--- src/Games/BlackJack.cpp
+++ src/Games/BlackJack.cpp
@@ -30,6 +30,8 @@ void BlackJack::Draw() {
   ClearBackground(BLUE);
   DrawPlayerHands();
   DrawDealerHands();
+  DrawLineEx({GlobalConstants::ScreenSize.x / 2.0f, 0},
+             {GlobalConstants::ScreenSize.x / 2.0f, GlobalConstants::ScreenSize.y}, 2, BLACK);
   EndDrawing();
 }
 
@@ -42,35 +44,50 @@ void BlackJack::Update() {
   }
 }
 
+// Draw Section
 void BlackJack::DrawPlayerHands() {
-  float OffsetX = 0;
+  float OffsetX     = 0;
+  float offsetDelta = 0.2;
+  OffsetX           = (offsetDelta * -1);
 
   for (Card card : player) {
-    float CardPosX =
-        GlobalConstants::ScreenSize.x * (0.5 + OffsetX) - (card.GetCardSize().x / 2.0f);
-    float CardPosY = GlobalConstants::ScreenSize.y * 0.5;
-    OffsetX += 0.3;
+    float CardPosX;
+    if (player.size() % 2 == 0) {
+      CardPosX =
+          GlobalConstants::ScreenSize.x * (0.5 + OffsetX) + (card.GetCardSize().x * 0.15); // centers cards on gap between
+    } else {
+      CardPosX =
+          GlobalConstants::ScreenSize.x * (0.5 + OffsetX) - card.GetCardSize().x * 0.5f; // center cards on center card
+    }
+    float CardPosY = GlobalConstants::ScreenSize.y * 0.55;
+    OffsetX += offsetDelta;
     card.DrawCard({CardPosX, CardPosY});
   }
   // std::cout << player.size() << "\n";
 }
 
 void BlackJack::DrawDealerHands() {
-  float Offset = 0.1;
+  float Offset      = 0.1;
+  float OffsetX     = 0;
+  float offsetDelta = 0.2;
+  OffsetX           = (offsetDelta * -1);
 
   for (Card card : dealer) {
-    float textPosY = GlobalConstants::ScreenSize.y * Offset;
-    float textPosX =
-        GlobalConstants::ScreenSize.x / 2.0f -
-        (MeasureTextEx(GetFontDefault(), card.GetCardName().c_str(), GlobalConstants::FontSize, 1)
-             .x) /
-            2.0f;
-    DrawTextEx(GetFontDefault(), card.GetCardName().c_str(), {textPosX, textPosY},
-               GlobalConstants::FontSize, 1, WHITE);
-    Offset += 0.06;
+    float CardPosX;
+    if (dealer.size() % 2 == 0) {
+      CardPosX =
+          GlobalConstants::ScreenSize.x * (0.5 + OffsetX) + (card.GetCardSize().x * 0.15); // centers cards on gap between
+    } else {
+      CardPosX =
+          GlobalConstants::ScreenSize.x * (0.5 + OffsetX) - card.GetCardSize().x * 0.5f; // center cards on center card
+    }
+    float CardPosY = GlobalConstants::ScreenSize.y * 0.05;
+    OffsetX += offsetDelta;
+    card.DrawCard({CardPosX, CardPosY});
   }
 }
 
+// Game Logic
 void BlackJack::DealerTurn() {
   DealerHandValue = 0;
   for (Card card : dealer) {
@@ -163,7 +180,8 @@ void BlackJack::DealCards(int turn) {
 }
 
 int BlackJack::GetNumDecks() {
-  std::cout << "Enter Number of Decks: ";
-  std::cin >> NumDecks;
+  // std::cout << "Enter Number of Decks: ";
+  // std::cin >> NumDecks;
+  NumDecks = 1;
   return NumDecks;
 }
